1cfe5abf810edb6afa574a7ff4b5c88138fc7736
okuaki <c0b2117339@edu.teu.ac.jp>
2022-10-04 16:46:47 +0900

    追加機能実装版

--- ex02/calc.py
+++ ex02/calc.py
@@ -3,50 +3,51 @@ import tkinter.messagebox as tkm
 
 def click_number(event):
     btn = event.widget
-    num = int(btn["text"])
+    num = btn["text"]
     #tkm.showinfo(f"{num}", f"{num}のボタンが押されました")
-    entry.insert(tk.END, num)#練習5
+    
+    if num == "=": #＝が押されたら計算
+        val = entry.get()
+        if val == "2501":
+            res = "GHOST IN THE SHELL"
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, res)
+        else:
+            res = eval(val)
+            entry.delete(0, tk.END)
+            entry.insert(tk.END, res)
+    elif num == "c":
+        entry.delete(0,tk.END)    
+    else:
+        entry.insert(tk.END, num)#練習5
+    
+
 
 
 #練習3
 
 root = tk.Tk()
-root.geometry("300x500")
+root.geometry("375x565")
 
+
+#入力ボタンの作成
 BUTTON=[
-    ["9","8","7"],
-    ["6","5","4"],
-    ["3","2","1"],
-    ["0","+"]
+    ["**"," "," ","c"],
+    ["9","8","7","*"],
+    ["6","5","4","-"],
+    ["3","2","1","/"],
+    ["+","0",".","="]
 ]
 
 for y, row in enumerate(BUTTON,1):
     for x, num in enumerate(row):
-        btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+        btn = tk.Button(root, text=str(num), font=("", 30), width=4, height=2)
         btn.bind("<1>", click_number)
         btn.grid(row=y, column=x)
 
-
-
-# for i in range(9, -1, -1):
-#     button = tk.Button(root, text=f"{i}", font=("Times New Roman", 30), width=4, height=2)
-#     button.grid()
-
 #入力欄の作成
-entry = tk.Entry(root, font=(",40"), width=25, justify= "right")
-entry.grid(row=0, column=0, columnspan=3)
+entry = tk.Entry(root, font=("",22), width=25,  justify= "right" )
+entry.grid(row=0, column=0, columnspan=4)
 
-# r, c = 1, 0
-# for i, num in enumerate(list(range(9,-1, -1))+["+"], 1):
-#     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
-#     btn.bind("<1>", click_number)
-#     btn.grid(row=r, column=c)
-#     c += 1
-#     if i%3 == 0:
-#         r += 1
-#         c = 0
-#練習2
-
-
-
+#ウィンドウ表示
 root.mainloop()
