073f1fb32f41b3e242f9d8284756cc1452ad75f8
okuaki <c0b2117339@edu.teu.ac.jp>
2022-10-04 14:59:16 +0900

    練習５改良版

--- ex02/calc.py
+++ ex02/calc.py
@@ -10,9 +10,23 @@ def click_number(event):
 
 #練習3
 
-
 root = tk.Tk()
 root.geometry("300x500")
+
+BUTTON=[
+    ["9","8","7"],
+    ["6","5","4"],
+    ["3","2","1"],
+    ["0","+"]
+]
+
+for y, row in enumerate(BUTTON,1):
+    for x, num in enumerate(row):
+        btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+        btn.bind("<1>", click_number)
+        btn.grid(row=y, column=x)
+
+
 
 # for i in range(9, -1, -1):
 #     button = tk.Button(root, text=f"{i}", font=("Times New Roman", 30), width=4, height=2)
@@ -22,15 +36,15 @@ root.geometry("300x500")
 entry = tk.Entry(root, font=(",40"), width=25, justify= "right")
 entry.grid(row=0, column=0, columnspan=3)
 
-r, c = 1, 0
-for i, num in enumerate(range(9,-1, -1), 1):
-    btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
-    btn.bind("<1>", click_number)
-    btn.grid(row=r, column=c)
-    c += 1
-    if i%3 == 0:
-        r += 1
-        c = 0
+# r, c = 1, 0
+# for i, num in enumerate(list(range(9,-1, -1))+["+"], 1):
+#     btn = tk.Button(root, text=f"{num}", font=("", 30), width=4, height=2)
+#     btn.bind("<1>", click_number)
+#     btn.grid(row=r, column=c)
+#     c += 1
+#     if i%3 == 0:
+#         r += 1
+#         c = 0
 #練習2
 
 
