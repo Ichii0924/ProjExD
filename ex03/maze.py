e6ee969d01d6d7c213315d35ab35320d81395d23
okuaki <c0b2117339@edu.teu.ac.jp>
2022-10-18 15:30:14 +0900

    練習12まで完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,4 +1,6 @@
 import tkinter as tk
+import maze_maker as mm #練習8
+
 
 #練習5
 def key_down(event):
@@ -12,15 +14,30 @@ def key_up(event):
 
 #練習7
 def main_proc():
+    global mx, my
     global cx, cy
+
     if key == "Up":
-        cy -= 20
+        my -= 1
     if key == "Down":
-        cy += 20
+        my += 1
     if key == "Left":
-        cx -= 20
+        mx -= 1
     if key == "Right":
-        cx += 20
+        mx += 1
+
+    if maze_lst[my][mx] == 0:
+        cx, cy = mx*100+50, my*100+50
+    else:
+        if key == "Up":
+            my += 1
+        if key == "Down":
+            my -= 1
+        if key == "Left":
+            mx += 1
+        if key == "Right":
+            mx -= 1        
+
     canv.coords("tori", cx, cy) 
     root.after(100, main_proc)
 
@@ -32,7 +49,11 @@ if __name__ == "__main__":
     canv = tk.Canvas(root, width=1500, height=900, bg="black")
     canv.pack()#練習2
 
+    maze_lst = mm.make_maze(15,9)#練習9
+    maze_up = mm.show_maze(canv, maze_lst)#練習10
+
     tori = tk.PhotoImage(file="fig/9.png")
+    mx, my = 1, 1
     cx, cy = 300, 400
     canv.create_image(cx, cy, image = tori, tag="tori")#練習3
 
